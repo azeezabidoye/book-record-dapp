6b0fc3ea22efbcf49f9bdbbcda52f5974193a244
azeezabidoye <azeezabidoye@yahoo.com>
2024-06-25 11:39:21 +0100

    Deploy script updated

--- deploy/00-deploy-book-record.cjs
+++ deploy/00-deploy-book-record.cjs
@@ -1,16 +1,18 @@
-const { verify } = require("../hardhat.config.cjs");
+const { verify } = require("../utils/verify.cjs");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy } = deployments;
+  const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+  const args = [];
   const bookRecord = await deploy("BookRecord", {
     contract: "BookRecord",
+    args: args,
     from: deployer,
     log: true, // Logs statements to console
   });
 
   if (process.env.ETHERSCAN_API_KEY) {
-    await verify(bookRecord.address);
+    await verify(bookRecord.target, args);
   }
   log("Contract verification successful...");
   log("............................................................");
