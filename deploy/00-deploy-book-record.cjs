aefff51ba5c1c28f34ba4815e09dd942db25018a
azeezabidoye <azeezabidoye@yahoo.com>
2024-06-25 11:29:35 +0100

    Config & deploy script updated

--- deploy/00-deploy-book-record.cjs
+++ deploy/00-deploy-book-record.cjs
@@ -1,10 +1,18 @@
+const { verify } = require("../hardhat.config.cjs");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy("BookRecord", {
+  const bookRecord = await deploy("BookRecord", {
     contract: "BookRecord",
     from: deployer,
     log: true, // Logs statements to console
   });
+
+  if (process.env.ETHERSCAN_API_KEY) {
+    await verify(bookRecord.address);
+  }
+  log("Contract verification successful...");
+  log("............................................................");
 };
 module.exports.tags = ["BookRecord"];
--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("hardhat-deploy");
+require("hardhat-gas-reporter");
 
 const { PRIVATE_KEY, INFURA_SEPOLIA_URL, ETHERSCAN_API_KEY } = process.env;
 
